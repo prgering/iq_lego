1c8f3b496d48efac097d56797296e5f91482875f
Paul Gering <pablogering@gmail.com>
2025-01-31 11:14:54 +0000

    tidy folder

--- LEGOv2/IQ_clean_data.py
+++ LEGOv2/IQ_clean_data.py
@@ -4,12 +4,18 @@ Loading and Cleaning LEGO Database
 """
 
 # Imports
+
 import pandas as pd
-from pathlib import Path
 import re
 import csv
+import json
+from pathlib import Path
+from analyse_sem_pas import SemParse
+
+
 
 # Functions
+
 def get_headers(txt_file, columns_to_add):
     """
     Function to extract column_names from readme.txt file and combine with
@@ -17,11 +23,14 @@ def get_headers(txt_file, columns_to_add):
     specified by Schmitt et al. (2011) or obvious from examination of csv file. 
     """
 
+    # Read lines from readme.txt
     with open(txt_file, 'r', encoding="latin1") as f:
         lines = f.readlines()
+    
+    # Extact column names from .txt
     column_names = []
     for line_number in range(66, 75):  
-        line = lines[line_number].strip().replace(",", "").replace("#", "Sum").replace("%", "Percent")
+        line = lines[line_number].strip().replace(",", "").replace("#", "Sum").replace("%", "Percent") # Clean column names
         words = line.split()
         for name in words:
             match = re.search(r'\((.*?)\)', name)
@@ -31,27 +40,27 @@ def get_headers(txt_file, columns_to_add):
                 column_names.append(f"Context{bracket_contents}{modified_string}")
             else:
                 column_names.append(name)
+
+    # Add FileCode as the first column
     column_names.insert(0, columns_to_add[0])
+
+    # Append the other column names to the end of the column_names list
     for columns in columns_to_add[1:]:
         column_names.append(columns)
+
     return column_names
 
-def drop_shifted_rows(data, columns):
-    """
-    Reading the csv resulted in a few rows recording data in the wrong cells, 
-    due to the reliance on a delimiter. This function removes the rows with
-    these problems, but it doesn't remove all the data from a single recording
-    """
 
-    for column in columns:
-        numerical_rows = data[data[column].apply(lambda x: str(x).replace('.', '', 1).isdigit())]
-        data = data.drop(numerical_rows.index)
-    return data
 
-def read_clean_write_data(data, column_names, removal_columns, dummy_columns, new_name):
+def read_clean_write_data(data, column_names, new_name, dummy_columns, drop_columns):
     """
     Function to read and clean the data from Schmitt et al. (2011)
+
+    CSV file is read into Python using delimiter. Column names added and incorrect category labels replaced. The SemanticParse column information is extracted
+    into seperate variables to be useable for the classifier.
     """
+    
+    # Reading csv file using delimiter (causes some shifting in data between columns)
     with open(data, 'r', encoding='latin1') as file:
         try:
             dialect = csv.Sniffer().sniff(file.read(1024))
@@ -60,15 +69,56 @@ def read_clean_write_data(data, column_names, removal_columns, dummy_columns, ne
         except csv.Error:
             delimiter = ';' 
             print(f"Default delimiter used: {delimiter}")
+    
     df = pd.read_csv(data, encoding='latin1', header=None, delimiter=delimiter)
+    
+    # Add headers to dataframe
     df.columns = column_names
-    df = drop_shifted_rows(df, columns = ["LoopName", "ExMo"])
+    
+    # Replace categories in variables with names from publication
+    replace_ASR = {'no input': 'timeout', 'no match': 'reject', 'complete': 'success'}
+    replace_modality = {'voice': 'speech'}   
+
+    df["ASRRecognitionStatus"] = df["ASRRecognitionStatus"].replace(replace_ASR)
+    df["Modality"] = df["Modality"].replace(replace_modality)
+     
+    # Drop Rows with null values
     null_values = ["", "nan","NA", "null", "None", "\\N"]
     df = df.replace(null_values, pd.NA, regex=False)
     df = df.dropna(subset=["IQAverage"])
-    df_dropped = df.drop(removal_columns, axis=1)
+    
+    # Extract the keys from the SemanticParse column and add to a dictionary
+    sem_parser = SemParse()
+    entity_counts = [sem_parser.count_entities(entry) for entry in df["SemanticParse"].dropna().tolist()]
+    with open('entity_counts.txt', 'w') as file:
+        file.write(json.dumps(entity_counts, indent=3, sort_keys=False)) 
+
+    # Extract keys from top-level of Semparse dictionary
+    first_keys = []
+    for dictionary in entity_counts:
+        if dictionary:
+            first_key = list(dictionary.keys())[0]
+            first_keys.append(first_key)
+        else: 
+            first_keys.append("")
+    unique_first_keys = list(set(first_keys))
+
+    # Add Boolean Categories relating to the top-level keys to dataframe
+    df.loc[df['SemanticParse'] == "Semantic no match", 'SemanticParse'] = "semantic_no_match"
+
+    for key in first_keys:
+        if key:
+            df[key + '_present'] = df['SemanticParse'].str.contains(r'\b' + re.escape(key) + r'\b', case=False, regex=True)
+        else: 
+            continue
+
+    # Drop unnecessary columns
+    df_dropped = df.drop(drop_columns, axis=1)
+    
+    # Convert Categorical Variables to Dummy Variables
     df_dummy = pd.get_dummies(data=df_dropped, columns=dummy_columns)
-    df_dummy.to_csv(new_name, index = False, header = True) 
+    
+    df_dummy.to_csv(new_name, index = False, header = True)
     return df_dummy
 
 # Code
@@ -77,17 +127,14 @@ if __name__ == "__main__":
     IQ_file = base_path / "csv/interactions.csv"
     New_file = base_path / "csv/clean_interactions.csv"
     Read_me = base_path.parent / "readme.txt"    
-    additional_col = ["FileCode", "WavFile", "EmotionState", "IQ1", "IQ2", "IQ3", "IQAverage"]
-    removal_columns = ['Prompt', 'Utterance', 'ASRRecognitionStatus', 'Modality', 'SemanticParse', 'Activity','SystemDialogueAct', 'UserDialogueAct', 'WavFile', 'EmotionState']
-    dummy_columns = ["ExMo", "ActivityType", "RoleName", "LoopName"]
+    add_col = ["FileCode", "WavFile", "EmotionState", "IQ1", "IQ2", "IQ3", "IQAverage"]
+    dummy_col = ["ASRRecognitionStatus", "ExMo", "Modality", "Activity", "ActivityType", "RoleName", "LoopName", "SystemDialogueAct", "UserDialogueAct", "EmotionState"]
+    drop_col = ['Prompt', 'Utterance', 'SemanticParse', 'WavFile']
 
-    column_names = get_headers(Read_me, additional_col)
-    cleaned_df = read_clean_write_data(IQ_file, column_names, removal_columns, dummy_columns, New_file)
+    column_names = get_headers(Read_me, add_col)
+    cleaned_df = read_clean_write_data(IQ_file, column_names, New_file, dummy_col, drop_col)
 
     # Check the dataframe for any missing data or errors
     for column in cleaned_df.columns:
         print(f"\nColumn: {column}")
         print(cleaned_df[column].unique()) 
-    
-
-    
