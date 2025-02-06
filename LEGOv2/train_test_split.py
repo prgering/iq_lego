030a937f66b5dc580c726d33685d108a77e9f733
Paul Gering <prgering1@sheffield.ac.uk>
2025-02-06 15:09:56 +0000

    run SVM with SMO and K-Fold Validation

--- LEGOv2/train_test_split.py
+++ LEGOv2/train_test_split.py
@@ -5,20 +5,29 @@ from pathlib import Path
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn import svm
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 from svm_smo.src.smo_optimizer import SVM
 
 def train_opt_split(df):
     x = df.drop("IQAverage", axis=1)
     y = df["IQAverage"]
-    return train_test_split(x, y, random_state=0, train_size = .60)
+    return train_test_split(x, y, random_state=42, train_size = .60)
 
 
 def train_svm(X, y):
-    #clf = svm.SVC(kernel='linear', C = 1.0).fit(X_train, y_train)
-    clf = SVM(kernel_type='linear').fit(X, y)
-    scores = cross_val_score(clf, X, y, cv=10, scoring='recall_macro')
-    print(scores)
+    macro_recall_scores = []
+    kf = KFold(n_splits=10, shuffle=True,random_state=42)
+    for i, (train_index, val_index) in enumerate(kf.split(X)):
+        X_train, X_val = X[train_index], X[val_index]
+        y_train, y_val = y[train_index], y[val_index]
+        clf = SVM(kernel_type='linear').fit(X_train, y_train)
+        y_pred = model.predict(X_val)
+        macro_recall = recall_score(y_val, y_pred, average='macro')
+        macro_recall_scores.append(macro_recall)
+        print(f"Fold {i} Macro-Recall: {macro_recall}")
+
+    average_macro_recall = np.mean(macro_recall_scores)
+    print(f"Average Macro-Recall across {k} folds: {average_macro_recall}")
 
     
 if __name__ == "__main__":
