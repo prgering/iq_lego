32bc7e1a9bdb1554bf8c4a960340b500e73ce8cc
Paul Gering <prgering1@sheffield.ac.uk>
2025-02-07 10:43:31 +0000

    Code to split data into feature types and train svm for each

--- LEGOv2/train_test_split.py
+++ LEGOv2/train_test_split.py
@@ -14,15 +14,44 @@ def train_opt_split(df):
     y = df["IQAverage"]
     return train_test_split(x, y, random_state=42, train_size = .60)
 
+def feature_dict(df):
+    feature_types = {}
+    keys = ["ASR", "SLU", "DM", "DAcT", "EMO"]
+    for key in keys:
+        feature_types[key] = []
+    
+    feature_types["ASR"].append(df.filter(regex='ASR|TimeOut|Barge|UTD|ExMo|WPUT|Modality').columns)
+    feature_types["DM"].append(df.filter(regex='WPST|DD|RoleIndex|Prompt|Exchange|Turns|SystemQuestions|Activity|Rolename|LoopName').columns)
+    feature_types["SLU"].append(df.filter(regex='present').columns)
+    feature_types["DAcT"].append(df.filter(regex='DialogueAct').columns)
+    feature_types["EMO"].append(df.filter(regex='EmotionState').columns)
+
+
+    return feature_types
+
+def split_features(df, feature_types):
+    dfs = {}
+    for key, columns_list in feature_types.items():
+        for columns in columns_list:
+            column_names = columns.tolist()  
+            dfs[key] = df[column_names]
+    df_AUTO = pd.concat([dfs["ASR"], dfs["SLU"], dfs["DM"]], axis=1, join='outer')
+    df_AUTOEMO = df.drop(dfs["EMO"].columns, axis=1)
+    breakpoint()
+
+    return dfs, df_AUTO, df_AUTOEMO
+
 
 def train_svm(X, y):
     macro_recall_scores = []
-    kf = KFold(n_splits=10, shuffle=True,random_state=42)
+    k = 10
+    kf = KFold(n_splits=k, shuffle=True,random_state=42)
     for i, (train_index, val_index) in enumerate(kf.split(X)):
         X_train, X_val = X[train_index], X[val_index]
         y_train, y_val = y[train_index], y[val_index]
-        clf = SVM(kernel_type='linear').fit(X_train, y_train)
-        y_pred = clf.predict(X_val)
+        clf = SVM(kernel_type='linear')
+        clf.fit(X_train, y_train)
+        y_pred, _ = clf.predict(X_val)
         macro_recall = recall_score(y_val, y_pred, average='macro')
         macro_recall_scores.append(macro_recall)
         print(f"Fold {i} Macro-Recall: {macro_recall}")
@@ -39,5 +68,22 @@ if __name__ == "__main__":
 
     X_train, X_opt, y_train, y_opt = train_opt_split(data)
 
-    train_svm(X_train.to_numpy(), y_train.to_numpy())
+    feature_type_dict = feature_dict(data)
+
+    dict_feature_data, auto_data, autoemo_data = split_features(X_train, feature_type_dict)
+
+    for key, dataframe in dict_feature_data.items():
+        print(f"Training {key} feature data \n")
+        train_svm(dataframe.to_numpy(), y_train.to_numpy())
+    
+    print("Training AUTO feature data \n")
+
+    train_svm(auto_data.to_numpy(), y_train.to_numpy())
+
+    print("Training AUTOEMO feature data \n")
+
+    train_svm(autoemo_data.to_numpy(), y_train.to_numpy())
+
+    print("Training ALL feature data \n")
+
 
