c33f4a256af73d2058b87f3ab25231443467e302
Paul Gering <prgering1@sheffield.ac.uk>
2025-02-11 13:20:06 +0000

    Code tidied and function descriptions added

--- LEGOv2/train_svm.py
+++ LEGOv2/train_svm.py
@@ -1,6 +1,15 @@
+"""
+
+Creating Train/Optimisation split and training an SVC
+
+"""
+
+# Imports
+
 import pandas as pd
 import numpy as np
 from pathlib import Path
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
@@ -9,13 +18,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 
 
+# Functions
 
 def train_opt_split(df):
     x = df.drop("IQAverage", axis=1)
     y = df["IQAverage"]
     return train_test_split(x, y, random_state=42, train_size = .60)
 
+
 def feature_dict(df):
+    """
+    Creates dictionary of input features arranged under keys based on the feature type
+    """
+
     feature_types = {}
     keys = ["ASR", "SLU", "DM", "DAcT", "EMO"]
     for key in keys:
@@ -30,7 +45,11 @@ def feature_dict(df):
 
     return feature_types
 
+
 def split_features(df, feature_types):
+    """
+    Splits features in a dataframe according to a dictionary of feature types
+    """
     dfs = {}
     for key, columns_list in feature_types.items():
         for columns in columns_list:
@@ -41,7 +60,11 @@ def split_features(df, feature_types):
 
     return dfs, df_AUTO, df_AUTOEMO
 
+
 def dim_reduce(df, n_components = 50, prefix=""):
+    """
+    Uses Principal Components Analysis to reduce the components of the word embedding features
+    """
     scaler = StandardScaler()
     df_scaled = scaler.fit_transform(df) 
     pca = PCA(n_components=n_components)
@@ -50,7 +73,11 @@ def dim_reduce(df, n_components = 50, prefix=""):
     df_with_heads = pd.DataFrame(df_reduced, index=df.index, columns=new_cols)
     return df_with_heads, pca, scaler
 
+
 def train_svm(X, y):
+    """
+    Trains an SVC on X to predict y using Stratified 10-fold Cross-Validation
+    """
     macro_recall_scores = []
     k = 10
     kf = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
@@ -91,11 +118,13 @@ if __name__ == "__main__":
     X_train, X_opt, y_train, y_opt = train_opt_split(data)
 
     embedding_prefixes = ["grammar_emb", "utterance_emb", "triggered_grammar"]
-
     best_recall = 0
     best_n = 0
 
+    # Test different parameter sizes
     for n in [25,50,75,100]:
+
+        # Reduce Dimensions for each embedding variable
         reduced_embeddings = []
         for prefix in embedding_prefixes:
             filtered_cols = [col for col in X_train if col.startswith(prefix)]
@@ -105,9 +134,11 @@ if __name__ == "__main__":
         all_reduced = pd.concat(reduced_embeddings, axis=1)
         X_train_reduced = pd.concat([X_train.drop([col for col in X_train if any(emb in col for emb in embedding_prefixes)], axis=1), all_reduced], axis=1)
 
-        feature_type_dict_reduced = feature_dict(X_train_reduced)
-        X_train_dict_reduced, X_train_auto_reduced, X_train_autoemo_reduced = split_features(X_train_reduced, feature_type_dict_reduced)
+        # Create Dictionary of feature types and split data based on feature types
+        feature_type_dict = feature_dict(X_train_reduced)
+        X_train_dict_reduced, X_train_auto_reduced, X_train_autoemo_reduced = split_features(X_train_reduced, feature_type_dict)
 
+        # Train SVM on different feature sets
         print(f"Training ALL feature data with {n} embedding dimensions\n")
         recall = train_svm(X_train_reduced, y_train)
 
