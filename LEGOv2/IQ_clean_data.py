3342737ef992db7b32523965b636cb48293694a5
Paul Gering <prgering1@sheffield.ac.uk>
2025-02-13 13:59:39 +0000

    Entering Data Types for columns and managing missing data

--- LEGOv2/IQ_clean_data.py
+++ LEGOv2/IQ_clean_data.py
@@ -1,4 +1,5 @@
-""" 
+
+    """ 
 Loading, Cleaning and Pre-processing the LEGO Database for classification
 """
 
@@ -43,13 +44,11 @@ def get_headers(txt_file, columns_to_add):
     return column_names
 
 
-def read_data(data, col_names):
+def read_data(data, col_names, null_values):
     """
     Function to read LEGO data from interaction.csv
     CSV file is read into Python using delimiter
     """
-    
-    null_values = ["", "nan","NA", "null", "None", "\\N"]
 
     delimiter = ';'
     
@@ -130,29 +129,64 @@ def extract_sentence_embeddings(data, column, prefix):
     return embeddings_df
 
 
-def clean_data(df, dummy_columns, drop_columns):
+def clean_data(df, dummy_columns, drop_columns, null_values):
     """
+    Function to clean the LEGO dataset
     """
+    # Address malformed rows from reading csv
     df = manage_malformed_rows(df)
 
-    df["ASRRecognitionStatus"] = df["ASRRecognitionStatus"].replace('"', '')
-    df["ExMo"] = df["ExMo"].replace('"', '')
+    # Identify NAs from malformed rows
+    df = df.replace(null_values, pd.NA, regex=False)
 
+    # Specify data types for numeric columns
+    numeric_cols_index = df.filter(regex='Sum|Context|Percent|Confidence|UTD|WPUT|WPST|DD|RoleIndex').columns
+
+    num_cols_list = numeric_cols_index.to_list()
+
+    df[num_cols_list] = df[num_cols_list].apply(pd.to_numeric, errors='coerce')
+
+    # Specify data types for string columns
+
+    string_cols_list = df.select_dtypes(include='object').columns.tolist() # Select columns of 'object' dtype (strings)
+    df[string_cols_list] = df[string_cols_list].astype(str)
+
+    # Address inconsistent category names and strings in multiple columns
+
+    df["ASRRecognitionStatus"] = df["ASRRecognitionStatus"].str.replace('"', '')
+    df["ExMo"] = df["ExMo"].str.replace('"', '')
+    df["Utterance"] = df["Utterance"].str.replace('"', '')
 
     replace_ASR = {'no input': 'timeout', 'no match': 'reject', 'complete': 'success'}
     replace_modality = {'voice': 'speech'}   
 
-    df["ASRRecognitionStatus"] = df["ASRRecognitionStatus"].replace(replace_ASR)
+    df["ASRRecognitionStatus"] = df["ASRRecognitionStatus"].replace(replace_ASR).str.replace(r"^$", "no status", regex=True)
     df["Modality"] = df["Modality"].replace(replace_modality)
-    
-    df = df.drop(df.loc[df['SystemDialogueAct'].isin(["SDA_GREETING", "SDA_OFFERHELP"])].index)
+
+    # Identify NA's after specifying data types
+
+    df = df.replace(null_values, pd.NA, regex=False)
+
+    # Remove NAs in IQAverage Column
+
+    df = df[df['IQAverage'].notna()]
+
+    # Replace Utterance NAs with empty quotes
+
+    df["Utterance"] = df["Utterance"].fillna("")
+
+    # Drop first turn of each interaction
+
+    df = df.drop(df.loc[df['SystemDialogueAct'].isin(["SDA_GREETING"])].index)
     df = df.reset_index(drop=True)
 
-    null_values = ["", "nan","NA", "null", "None", "\\N"]
-    
-    df = df.replace(null_values, pd.NA, regex=False)
-    df = df.astype(str).replace('nan', pd.NA)
+    # Remove decimal places from categorical variable labels
 
+    for col in df.columns:
+        if df[col].dtype == "object":
+            df[col] = df[col].replace(r'\.0$', '', regex=True)
+
+    # Extract Semantic Parse information into seperate binary variables
     sem_keys = extract_keys_sem_parse(df)
     df.loc[df['SemanticParse'] == "Semantic no match", 'SemanticParse'] = "semantic_no_match"
 
@@ -161,16 +195,16 @@ def clean_data(df, dummy_columns, drop_columns):
             df[key + '_present'] = df['SemanticParse'].str.contains(r'\b' + re.escape(key) + r'\b', case=False, regex=True).replace(pd.NA, False)
         else: 
             continue
-
+    
+    # Create Word Embeddings for Utterance Column
     embed_utterance = extract_sentence_embeddings(df, "Utterance", prefix= "utterance_emb")
     df_with_embeddings = pd.concat([df, embed_utterance], axis = 1)
 
+    # Drop columns unnecessary for classification
+
     df_dropped = df_with_embeddings.drop(drop_columns, axis=1)
 
-    df_dropped["IQAverage"] = df_dropped["IQAverage"].fillna("0")
-    
-    for col in df_dropped.columns:
-        df_dropped[col] = df_dropped[col].astype(str).replace(r'\.0$', '', regex=True)
+    # Dummy Coding
 
     df_dummy = pd.get_dummies(data=df_dropped, columns=dummy_columns)
 
@@ -182,20 +216,20 @@ def clean_data(df, dummy_columns, drop_columns):
 if __name__ == "__main__":
     base_path = Path("/home/acp23prg/fastdata/projects/iq_lego/LEGOv2/corpus")
     IQ_file = base_path / "csv/interactions.csv"
-    new_file = base_path / "csv/clean_interactions_all.csv"
+    new_file = base_path / "csv/clean_interactions.csv"
     read_me = base_path.parent / "readme.txt" 
+
+    null_vals = ["", "<NA>", "nan","NA", "null", "None", "\\N", " "]
 
     add_col = ["FileCode", "WavFile", "EmotionState", "IQ1", "IQ2", "IQ3", "IQAverage"]
     dummy_col = ["ASRRecognitionStatus", "ExMo", "Modality", "Activity", "ActivityType", "RoleName", "LoopName", "SystemDialogueAct", "UserDialogueAct", "EmotionState"]
-    drop_col = ['Prompt', 'Utterance', 'SemanticParse', 'WavFile', "IQ1", "IQ2", "IQ3", "HelpRequest?", "SumHelpRequests", "ContextSumHelpRequest", "PercentHelpRequest"]
+    drop_col = ['Prompt','Utterance','SemanticParse', 'WavFile', "IQ1", "IQ2", "IQ3", "HelpRequest?", "SumHelpRequests", "ContextSumHelpRequest", "PercentHelpRequest"]
 
     column_names = get_headers(read_me, add_col)
-    uncleaned_df = read_data(IQ_file, column_names)
-    cleaned_df = clean_data(uncleaned_df, dummy_col, drop_col)
+    uncleaned_df = read_data(IQ_file, column_names, null_vals)
+    cleaned_df = clean_data(uncleaned_df, dummy_col, drop_col, null_vals)
     cleaned_df.to_csv(new_file, index = False, header = True)
 
     for column in cleaned_df.columns:
         print(f"\nColumn: {column}")
         print(cleaned_df[column].unique()) 
-    
-    print(cleaned_df.isna().sum())
